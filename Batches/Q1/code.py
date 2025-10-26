43e0a3b81722fd6a53605409ba1a8bc37b6bf971
BharathiV2004 <bharathi.blue005@gmail.com>
2025-10-26 14:16:03 +0530

    Update code.py

--- Batches/Q1/code.py
+++ Batches/Q1/code.py
@@ -1,1 +1,33 @@
-
+from typing import List
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        """
+        Finds the smallest path sum from top to bottom in a triangle.
+        We start from the bottom and work our way up, updating the minimum
+        sums along the way.
+        """
+        if not triangle:
+            # If the triangle is empty, there’s nothing to sum
+            return 0
+        # Make a copy of the last row. Think of this as our "running totals".
+        running_totals = triangle[-1].copy()
+        # Go from the second-to-last row up to the top
+        for row_index in range(len(triangle) - 2, -1, -1):
+            for col_index in range(len(triangle[row_index])):
+                # For each number, pick the smaller path from the two numbers below it
+                running_totals[col_index] = (
+                    triangle[row_index][col_index] 
+                    + min(running_totals[col_index], running_totals[col_index + 1])
+                )
+        # By the time we reach the top, the first element has the minimum path sum
+        return running_totals[0]
+# example:
+if __name__ == "__main__":
+    triangle = [
+        [2],
+        [3, 4],
+        [6, 5, 7],
+        [4, 1, 8, 3]
+    ]
+    solution = Solution()
+    print("The minimum path sum is:", solution.minimumTotal(triangle))  # Should print 11
