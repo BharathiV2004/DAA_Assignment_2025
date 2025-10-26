d38f0551ef26ecddd319587697a138ce82161b21
BharathiV2004 <bharathi.blue005@gmail.com>
2025-10-26 14:42:37 +0530

    Update code.py

--- Batches/Q2/code.py
+++ Batches/Q2/code.py
@@ -1,1 +1,31 @@
 
+from typing import List
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        #Returns all possible sentences where the string `s` can be segmented into words from `wordDict`.
+        # Convert the word list into a set for O(1) lookups
+        word_set = set(wordDict)
+        memo = {}  # start index -> list of sentences
+        def dfs(start: int) -> List[str]:
+            if start == len(s):
+                # Reached the end of the string: return a list with an empty string
+                return [""]
+            if start in memo:
+                # Return already computed results for this start index
+                return memo[start]
+            sentences = []
+            # Try every possible end index
+            for end in range(start + 1, len(s) + 1):
+                word = s[start:end]
+                if word in word_set:
+                    # Get all sentences for the substring starting at `end`
+                    for suffix in dfs(end):
+                        if suffix:
+                            # If there is more string after current word, add a space
+                            sentences.append(word + " " + suffix)
+                        else:
+                            sentences.append(word)
+            # Memoize the result for this start index
+            memo[start] = sentences
+            return sentences
+        return dfs(0)
